f559ba0762a2693b7d2092c310171b49b9532033
taewoooh <taewoooh88@gmail.com>
2018-03-05 23:03:24 +0900

    비밀번호 재설정 셋티웆ㅇ

--- app/src/main/java/com/example/lg/twcircleapp/ResetPasswordActivity.java
+++ app/src/main/java/com/example/lg/twcircleapp/ResetPasswordActivity.java
@@ -2,12 +2,65 @@ package com.example.lg.twcircleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class ResetPasswordActivity extends AppCompatActivity {
+
+    ImageView backimage,delete;
+    MaterialEditText editid;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+
+        backimage =(ImageView)  findViewById(R.id.backimage);
+        delete = (ImageView) findViewById(R.id.delete);
+        editid = (MaterialEditText) findViewById(R.id.editid);
+
+        editid.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                    if (s.length() > 0){
+
+                        delete.setVisibility(View.VISIBLE);
+                    }else if (s.length() == 0){
+                        delete.setVisibility(View.INVISIBLE);
+                    }
+            }
+        });
+
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editid.setText(null);
+
+            }
+        });
+
+        backimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
