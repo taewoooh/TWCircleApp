89692de3ef709b27b377ba9d74ef64e437a14b33
taewoooh <taewoooh88@gmail.com>
2018-03-08 00:07:26 +0900

    메인화면 Bottom 네비게이션바 개발시작

--- app/src/main/java/com/example/lg/twcircleapp/MainActivity.java
+++ app/src/main/java/com/example/lg/twcircleapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.lg.twcircleapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+
+                    case R.id.home:
+                        Toast.makeText(MainActivity.this,"action add clicked",Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.search:
+                        Toast.makeText(MainActivity.this,"action edit clicked",Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.like:
+                        Toast.makeText(MainActivity.this,"action remove clicked",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.myprofile:
+                        Toast.makeText(MainActivity.this,"action remove clicked",Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+            }
+        });
     }
 }
