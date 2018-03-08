fa773765f2aa0f637ed198aad0ab8e7de975dada
taewoooh <taewoooh88@gmail.com>
2018-03-08 21:22:35 +0900

    메인화면 Bottom 네비게이션바 개발시작

--- app/src/main/java/com/example/lg/twcircleapp/MainActivity.java
+++ app/src/main/java/com/example/lg/twcircleapp/MainActivity.java
@@ -1,42 +1,55 @@
 package com.example.lg.twcircleapp;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        mTextMessage = (TextView) findViewById(R.id.message);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+
         bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
             @Override
             public void onNavigationItemReselected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
 
-                    case R.id.home:
-                        Toast.makeText(MainActivity.this,"action add clicked",Toast.LENGTH_SHORT).show();
+                    case R.id.ic_home:
+                        Toast.makeText(MainActivity.this, "home", Toast.LENGTH_SHORT).show();
                         break;
 
-                    case R.id.search:
-                        Toast.makeText(MainActivity.this,"action edit clicked",Toast.LENGTH_SHORT).show();
+                    case R.id.ic_search:
+                        Toast.makeText(MainActivity.this, "search", Toast.LENGTH_SHORT).show();
                         break;
 
-                    case R.id.like:
-                        Toast.makeText(MainActivity.this,"action remove clicked",Toast.LENGTH_SHORT).show();
+                    case R.id.ic_message:
+                        Toast.makeText(MainActivity.this, "message", Toast.LENGTH_SHORT).show();
                         break;
-                    case R.id.myprofile:
-                        Toast.makeText(MainActivity.this,"action remove clicked",Toast.LENGTH_SHORT).show();
+                    case R.id.ic_profile:
+                        Toast.makeText(MainActivity.this, "profile", Toast.LENGTH_SHORT).show();
                         break;
 
                 }
             }
         });
+
+
     }
+
 }
